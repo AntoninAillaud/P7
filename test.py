321789de695a6ed6fc59cda82edff5f3dfb5fea6
Antonin Aillaud <spyre_aa@hotmail.com>
2024-07-10 18:53:08 +0200

    Modification des tests unitaires

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import pytest
 import requests
+from fastapi.testclient import TestClient
 
 from api import app
+client = TestClient(app)
 
 def test_predict_accept():
-    res = requests.post("http://127.0.0.1:8000/predict/10")
+    res = client.post("/predict/10")
     assert res.status_code == 200
     assert res.text == '"Acceptée"'
     
 def test_predict_reject():
-    res = requests.post("http://127.0.0.1:8000/predict/11")
+    res = client.post("/predict/11")
     assert res.status_code == 200
     assert res.text == '"Rejetée"'
