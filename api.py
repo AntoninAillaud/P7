f80e881e07601598cd04b029d2f68547cf1cb4cb
Antonin Aillaud <spyre_aa@hotmail.com>
2024-07-10 17:15:52 +0200

    Ajout de l'api en .py

---
+++ api.py
@@ -0,0 +1,19 @@
+#FAST_API
+import pandas as pd
+import dill as pickle
+from fastapi import FastAPI
+import uvicorn
+
+best_model = pickle.load(open('./BEST_MODEL.sav', 'rb'))
+f = open("./seuil.txt","r")
+seuil = float(f.read())
+f.close()
+
+df = pd.read_csv('./test.csv')
+
+app = FastAPI()
+
+@app.post("/predict/{idx}")
+async def predict(idx: int):
+    res = best_model.predict_proba(df.values[idx].reshape(1,-1))[:,1]
+    return "Rejetée" if res > seuil else 'Acceptée'
