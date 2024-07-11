38cc39ec2677af72e582c43676058c5e39451f59
Antonin Aillaud <spyre_aa@hotmail.com>
2024-07-11 14:23:30 +0200

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import pandas as pd
 import dill as pickle
 from fastapi import FastAPI
 import uvicorn
+import nest_asyncio
 
 best_model = pickle.load(open('./BEST_MODEL.sav', 'rb'))
 f = open("./seuil.txt","r")
@@ -19,4 +20,5 @@ async def predict(idx: int):
     return "Rejetée" if res > seuil else 'Acceptée'
 
 if __name__ == '__main__':
+    nest_asyncio.apply()
     uvicorn.run(app, host='0.0.0.0', port="8000")
