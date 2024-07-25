4f82beaab49ecbda6b3d6b45b4c5ca7e48d7e749
Antonin Aillaud <spyre_aa@hotmail.com>
2024-07-25 12:49:31 +0200

    Update api.py

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ import dill as pickle
 from fastapi import FastAPI
 import uvicorn
 import nest_asyncio
+import json
+from pydantic import BaseModel
 
 #Récupération du modèle et du seuil
 best_model = pickle.load(open('./BEST_MODEL.sav', 'rb'))
@@ -14,18 +16,23 @@ f.close()
 #Récupération des données
 df = pd.read_csv('./test.csv')
 
+class User_input(BaseModel):
+    idx : int
+
 app = FastAPI()
 
 #Affichage d'un message simple pour la page d'accueuil
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World"} 
 
 #Affichage si pour un client donné (idx) le modèle et le seuil accepte ou non sa demande de crédit
-@app.post("/predict/{idx}")
-async def predict(idx: int):
-    res = best_model.predict_proba(df.values[idx].reshape(1,-1))[:,1]
-    return "Rejetée" if res > seuil else 'Acceptée'
+@app.post("/predict")
+async def predict(input:User_input):
+    res = best_model.predict_proba(df.values[input.idx].reshape(1,-1))[:,1]
+    rep = "Rejetée" if res[0] > seuil else 'Acceptée'
+    d = [{'rep' : rep, 'proba' : res[0]}]
+    return d
 
 if __name__ == '__main__':
     nest_asyncio.apply()
